d10dfdaaba63ac825cd34db3f2a30c525d57c6f8
Hizhniy <hizhniy@gmail.com>
2023-07-29 18:35:01 +0300

    Добавьте файлы проекта.

---
+++ Practices.Tests/CalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Practices.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Additional_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            NUnit.Framework.Assert.That(calculator.Additional(-100, 100) == 0);
+        }
+
+        [Test]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            NUnit.Framework.Assert.That(calculator.Subtraction(100, 200) == -100);
+        }
+
+        [Test]
+        public void Miltiplication_MustReturnCorrectValue()
+        {
+            var calculatorTest = new Calculator();
+            NUnit.Framework.Assert.That(calculatorTest.Miltiplication(3, 100) == 300);
+        }
+
+        [Test]
+        public void Division_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            NUnit.Framework.Assert.That(calculator.Division(90, -2) == -45);
+        }
+
+        [Test]
+        public void Division_MustThrowException()
+        {
+            var calculator = new Calculator();
+            NUnit.Framework.Assert.Throws<DivideByZeroException>(() => calculator.Division(100, 0));
+        }
+    }
+}
